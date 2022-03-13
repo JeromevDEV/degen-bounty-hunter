639903e5e03195e7198f13f8de5df6c62fc55022
JeromevDEV <jeromev.dev@hotmail.com>
2022-03-13 18:38:44 +0100

    fix

--- src/components/TeamCards.jsx
+++ src/components/TeamCards.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { BiLinkExternal } from "react-icons/bi";
 
 function TeamCards(props) {
   return (
@@ -12,9 +10,6 @@ function TeamCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        {/*<Button variant="primary" href={props.link} target="_blank">
-          <BiLinkExternal /> See more;
-        </Button>*/}
       </Card.Body>
     </Card>
   );
