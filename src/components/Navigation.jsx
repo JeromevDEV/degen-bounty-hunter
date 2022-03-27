357a5f8c79d5315187b899ef050241237a85e1bf
JeromevDEV <jeromev.dev@hotmail.com>
2022-03-27 21:48:09 +0200

    responsive header & footer

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,57 +1,107 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import {
-  AiOutlineHome,
-  AiOutlineUser,
-  AiOutlineBook,
-  AiOutlineFileUnknown,
-  AiOutlineThunderbolt
+import React, { useState }  from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
+import {NavLink} from "react-router-dom";
+import {
+    AiOutlineHome,
+    AiOutlineUser,
+    AiOutlineBook,
+    AiOutlineFileUnknown,
+    AiOutlineThunderbolt
 } from "react-icons/ai";
 
-
 function Navigation() {
-  return (
-    <div className="navigation">
-      <nav className="navbar navbar-expand ">
-        <div className="container">
-          <NavLink className="navbar-brand" to="/">
-            DEGEN BOUNTY HUNTER
-          </NavLink>
-          <div>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/">
-                  <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-                  <span className="sr-only">(current)</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/hunt">
-                  <AiOutlineThunderbolt style={{ marginBottom: "2px" }} /> Hunt
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/roadmap">
-                  <AiOutlineFileUnknown style={{ marginBottom: "2px" }} /> Roadmap
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/whitepaper">
-                  <AiOutlineBook style={{ marginBottom: "2px" }} /> White paper
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
-                  <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
+
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
+    }
+  }
+  window.addEventListener("scroll", scrollHandler);
+
+    return (
+        <Navbar
+            expanded={expand}
+            fixed="top"
+            expand="md"
+            className={navColour ? "sticky" : "navbar"}
+        >
+          <Container>
+            <Navbar.Brand href="/" className="d-flex">
+              <p style={{marginTop: "15px"}}>Degen Bounty Hunter</p>
+            </Navbar.Brand>
+            <Navbar.Toggle
+                aria-controls="responsive-navbar-nav"
+                onClick={() => {
+                  updateExpanded(expand ? false : "expanded");
+                }}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ms-auto" defaultActiveKey="#home">
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                    <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                  </Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link
+                      as={Link}
+                      to="/hunt"
+                      onClick={() => updateExpanded(false)}
+                  >
+                    <AiOutlineThunderbolt  style={{ marginBottom: "2px" }} /> Hunt
+                  </Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link
+                      as={Link}
+                      to="/project"
+                      onClick={() => updateExpanded(false)}
+                  >
+                    <AiOutlineFileUnknown
+                        style={{ marginBottom: "2px" }}
+                    />{" "}
+                    Roadmap
+                  </Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link
+                      as={Link}
+                      to="/Whitepaper"
+                      onClick={() => updateExpanded(false)}
+                  >
+                    <AiOutlineBook  style={{ marginBottom: "2px" }} /> White paper
+                  </Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link
+                      as={Link}
+                      to="/about"
+                      onClick={() => updateExpanded(false)}
+                  >
+                    <AiOutlineUser  style={{ marginBottom: "2px" }} /> About
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+    );
 }
 
 export default Navigation;
