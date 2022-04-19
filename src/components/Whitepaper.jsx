4e6d3736b43263b468bd6a700eaf6c8c35debaec
JeromevDEV <jeromev.dev@hotmail.com>
2022-04-19 14:56:42 +0200

    wip ytb

--- src/components/Whitepaper.jsx
+++ src/components/Whitepaper.jsx
@@ -5,7 +5,7 @@ import wp_2 from "../Assets/wp_2.png";
 import wp_3 from "../Assets/wp_3.png";
 import wp_4 from "../Assets/wp_4.png";
 import wp_5 from "../Assets/wp_5.png";
-import YoutubeEmbed from "./YoutubeEmbed";
+import YouTube from 'react-youtube';
 
 function Whitepaper() {
     return (
@@ -16,7 +16,9 @@ function Whitepaper() {
                         You asked for the <strong className="main-name"> White-paper </strong> ?
                     </h1>
                     <Row>
-                        <YoutubeEmbed embedId="rokGy0huYEA" />
+                        <Col md={12}>
+                            <YouTube videoId="0iYsBnj2BUk"/>
+                        </Col>
                     </Row>
                     <Row>
                         <Col md={12}>
