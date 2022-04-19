d54fb5bcccf30fa9ba07743950d5cc4fa2b695ac
JeromevDEV <jeromev.dev@hotmail.com>
2022-04-19 15:35:21 +0200

    wip ytb

--- src/components/Whitepaper.jsx
+++ src/components/Whitepaper.jsx
@@ -5,54 +5,8 @@ import wp_2 from "../Assets/wp_2.png";
 import wp_3 from "../Assets/wp_3.png";
 import wp_4 from "../Assets/wp_4.png";
 import wp_5 from "../Assets/wp_5.png";
-import YouTube from 'react-youtube'
-
-var cElement = null;
-
-function Pause(props) {
-    return <button onClick={props.handleClick}>Pause</button>;
-}
-
-function Video(props) {
-    console.log(props);
-    const opts = {
-        height: "390",
-        width: "640",
-        playerVars: {
-            // https://developers.google.com/youtube/player_parameters
-            autoplay: 1
-        }
-    };
-
-    useEffect(() => {
-        if (cElement) {
-            props.isPaused
-                ? cElement.target.pauseVideo()
-                : cElement.target.playVideo();
-        }
-    }, [props.isPaused]);
-
-    const _onReady = event => {
-        console.log("_onReady");
-        cElement = event;
-        // event.target.playVideo();
-    };
-
-    const _onStateChange = event => {
-        // event.target.pauseVideo()
-    };
-    return (
-        <YouTube
-            videoId={"21X5lGlDOfg"}
-            opts={opts}
-            onReady={_onReady}
-            onStateChange={_onStateChange}
-        />
-    );
-}
 
 function Whitepaper() {
-    const [isPaused, setIsPaused] = useState(false);
 
     return (<section>
             <Container fluid className="whitepaper-section" id="whitepaper">
