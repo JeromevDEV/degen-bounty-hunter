1ddbb5b7c1a412085271a93385a1855f1b0135a6
JeromevDEV <jeromev.dev@hotmail.com>
2022-04-20 09:54:46 +0200

    wip ytb

--- src/components/YoutubePlayer.jsx
+++ src/components/YoutubePlayer.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import YouTube from 'react-youtube';
 
 
 function YoutubePlayer() {
     return (
         <div>
-            <iframe width="1120" height="630" src="https://www.youtube.com/embed/uqv-pDtKHSE"
-    title="YouTube video player" frameBorder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen/>
+            <YouTube videoId="uqv-pDtKHSE"/>;
         </div>
+
     );
 }
 
