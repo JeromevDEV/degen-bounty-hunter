0bf014e3c1df0e93245bd8ceaab505b961b3698d
JeromevDEV <jeromev.dev@hotmail.com>
2022-04-20 09:52:03 +0200

    wip ytb

--- src/components/YoutubePlayer.jsx
+++ src/components/YoutubePlayer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function YoutubePlayer() {
     return (
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/uqv-pDtKHSE"
+            <iframe width="1120" height="630" src="https://www.youtube.com/embed/uqv-pDtKHSE"
     title="YouTube video player" frameBorder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowFullScreen/>
