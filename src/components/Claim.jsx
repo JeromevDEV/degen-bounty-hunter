c48733dbc0250eb588182596bad114cc44130ad4
JeromevDEV <jeromev.dev@hotmail.com>
2022-05-29 20:52:27 +0200

    fix typo

--- src/components/Claim.jsx
+++ src/components/Claim.jsx
@@ -13,7 +13,7 @@ function Claim() {
     const wallet = useWallet();
     const {publicKey} = useWallet();
     const [arr, setArr] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [NFTs, setNFTs] = useState([]);
     const [mint, setMints] = useState();
     const API_KEY = "nxhv0sPzGRpNAkM";
