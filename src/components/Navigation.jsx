3a032d1e95d1f04502a93df7c32af27365bdee1c
JeromevDEV <jeromev.dev@hotmail.com>
2022-05-29 14:28:02 +0200

    clean

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -124,22 +124,9 @@ function Navigation() {
               </Nav.Link>
             </Nav.Item>
 
-            {/*                <Nav.Item>
-                  <Nav.Link>
-                    <WalletMultiButton />
-                  </Nav.Link>
-                </Nav.Item>*/}
             <Nav.Item>
               <Nav.Link>
-                {/* <button
-                  className="btn-primary"
-                  onClick={() => {
-                    // Phantom Wallet
-                    phantom_connect();
-                  }}
-                > */}
                 <WalletMultiButton/>
-                {/* </button> */}
               </Nav.Link>
             </Nav.Item>
           </Nav>
@@ -149,41 +136,4 @@ function Navigation() {
   );
 }
 
-function phantom_connect() {
-  // Check for Solana & Phantom
-  var provider = () => {
-    if ("solana" in window) {
-      var provider = window.solana;
-      if (provider.isPhantom) {
-        return provider;
-      } else {
-        return false;
-      }
-    }
-    window.open("https://phantom.app", "_blank");
-  };
-
-  var phantom = provider();
-
-  if (phantom !== false) {
-    console.log("Phantom Wallet Found, Connecting..");
-
-    try {
-      // Connect to Solana
-      var connect_wallet = phantom.connect();
-
-      // After Connecting
-      phantom.on("connect", () => {
-        // Check Connection
-        console.log("Phantom Connected: " + phantom.isConnected);
-
-        // Get Wallet Address
-        var wallet_address = phantom.publicKey.toString();
-        console.log("Solana Wallet Address: " + wallet_address);
-      });
-    } catch (err) {
-      console.log("Connection Cancelled!");
-    }
-  }
-}
 export default Navigation;
